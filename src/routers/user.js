e8890cd70cb0c1cdd2e8e2ab5112f1e81a776f60
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-13 18:51:26 +0100

    updated to new pattern

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 const auth = require('../util/auth');
@@ -8,38 +7,45 @@ const AuthorizationController = require('../controllers/AuthorizationController'
 
 const router = express.Router();
 
-// curd 
+// curd
 
 router.get('/', async (req, res) => {
-    const payload = req.body.payload;
-    if (payload.email) {
-        const user = await UserController.retriveOne(payload.email);
+    if (req.body.email) {
+        const user = await UserController.retriveOne(req.body.email);
         if (user) {
             await res.send(user, 200);
-        } 
+        }
     }
     await res.send({}, 400);
 });
 
 router.post('/login', async (req, res) => {
-    const payload = req.body.payload;
-    if (payload.email === undefined) {
-        await res.send({ errors: { email: 'This field is required' }});
+    if (req.body.email === undefined) {
+        await res.send({ errors: { email: 'This field is required' } });
         return;
     }
-    const user = await UserController.retriveOne(payload.email);
-    if (user && await AuthorizationController.verify(user, payload.password)) {
-        await res.send({ token: await auth.generateToken({ email: user.email })});
+    const user = await UserController.retriveOne(req.body.email);
+    if (
+        user &&
+        (await AuthorizationController.verify(user, req.body.password))
+    ) {
+        await res.send({
+            token: await auth.generateToken({ email: user.email }),
+        });
     } else {
-        await res.send({ error: 'USER_WITH_EMAIL_AND_PASSWORD_DOES_NOT_EXIST'});
+        await res.send({
+            error: 'USER_WITH_EMAIL_AND_PASSWORD_DOES_NOT_EXIST',
+        });
     }
 });
 
-router.post('/register', async (req, res) => { 
-    const payload = req.body.payload;
-    const user = await UserController.create(payload.email, payload.password, payload.phone);
+router.post('/register', async (req, res) => {
+    const user = await UserController.create(
+        req.body.email,
+        req.body.password,
+        req.body.phone
+    );
     await res.send(user);
 });
 
 module.exports = router;
-
